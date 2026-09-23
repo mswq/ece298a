5e37eafb69fd82d383a66cf3dc998dd6631958cb
mswq <90587894+mswq@users.noreply.github.com>
2026-09-23 19:06:15 -0400

    testing for 8 bit counter
    Updated test logic and assertions for DUT behavior.

--- test/test.py
+++ test/test.py
@@ -1,40 +1,71 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
+# SPDX-FileCopyrightText: Â© 2024 Tiny Tapeout
 # SPDX-License-Identifier: Apache-2.0
 
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import RisingEdge, Timer
 
 
 @cocotb.test()
 async def test_project(dut):
-    dut._log.info("Start")
-
-    # Set the clock period to 10 us (100 KHz)
+    # Start clock: 10 us period
     clock = Clock(dut.clk, 10, unit="us")
     cocotb.start_soon(clock.start())
 
-    # Reset
-    dut._log.info("Reset")
+    # Initial values
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
+
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+    await RisingEdge(dut.clk)
+    await Timer(1, unit="ns")
+
+    assert dut.uo_out.value == 0
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
+    await RisingEdge(dut.clk)
+    await Timer(1, unit="ns")
+    assert dut.uo_out.value == 1
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    await RisingEdge(dut.clk)
+    await Timer(1, unit="ns")
+    assert dut.uo_out.value == 2
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    dut.uio_in.value = 42
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    # ui_in[0] = load
+    # ui_in[1] = oe
+    #
+    # load = 1
+    # oe   = 0
+    dut.ui_in.value = 0b00000001
+    await RisingEdge(dut.clk)
+    await Timer(1, unit="ns")
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    assert dut.uo_out.value == 42
+
+    # load = 0
+    # oe   = 0
+    dut.ui_in.value = 0
+    await RisingEdge(dut.clk)
+    await Timer(1, unit="ns")
+
+    # oe = 0 -> uio pins should not be driven
+    assert dut.uio_oe.value == 0x00
+
+    # Turn OE on:
+    # load = 0
+    # oe   = 1
+    dut.ui_in.value = 0b00000010
+    await Timer(1, unit="ns")
+    assert dut.uio_oe.value == 0xFF
+    assert dut.uio_out.value == 43
+
+
+    # Turn OE back off
+    dut.ui_in.value = 0
+    await Timer(1, unit="ns")
+    assert dut.uio_oe.value == 0x00
+
+    dut._log.info("tests passed")
